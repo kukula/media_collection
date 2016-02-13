9c2bd75714a4e40de6697fd26ebfb6910d022718
Anatoliy Kukul <anatolkukula@gmail.com>
2016-02-13 13:43:42 +0100

    Implement sign in/out

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.feature "User authentication", type: :feature do
+  given!(:user) { FactoryGirl.create :user, email: "obiwan@kenobi.com" }
+
   scenario "As a user I want to be able to create an account" do
     visit "/"
 
@@ -11,5 +13,26 @@ RSpec.feature "User authentication", type: :feature do
     click_button "Sign up"
 
     expect(page).to have_text("Welcome! You have signed up successfully.")
+  end
+
+  scenario "As a user I want to be able to sign in" do
+    visit "/"
+
+    click_link "Sign in"
+    fill_in "Email", with: "obiwan@kenobi.com"
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_text("Signed in successfully.")
+  end
+
+  scenario "As a user I want to be able to sign out" do
+    login_as(user)
+    visit "/"
+
+    expect(page).to have_text("Signed in as obiwan@kenobi.com")
+    click_link "Sign out"
+
+    expect(page).to have_text("Signed out successfully.")
   end
 end
